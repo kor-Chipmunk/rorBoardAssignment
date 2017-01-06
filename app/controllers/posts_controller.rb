70562fc75421414bb6f1017f4b20555ee6b28a36
likelion_ksj <rhj4862@gmail.com>
2017-01-06 14:15:21 +0900

    Use Helpers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,72 +2,68 @@ class PostsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @Posts = Post.all.order(created_at: :desc)
+    @posts = Post.all.order(created_at: :desc)
   end
 
   def new
   end
 
   def create
-    title = params[:title]
-    detail = params[:detail]
-    
-    post = Post.new
-    post.title = title
-    post.detail = detail
+    post = Post.new(post_params)
+   
     if post.save
       flash[:success] = "게시글을 생성하셨습니다!"
-      redirect_to '/'
+      redirect_to posts_path
     else
       flash[:alert] = "게시글 생성에 실패하였습니다!"
-      redirect_to '/'
+      redirect_to posts_path
     end
   end
 
   def show
-    post_id = params[:post_id]
-    post = Post.find(post_id)
+    post = post_by_id
 
     @title = post.title
     @detail = post.detail
   end
 
   def edit
-    post_id = params[:post_id]
-    post = Post.find(post_id)
+    post = post_by_id
     
     @title = post.title
     @detail = post.detail
   end
 
   def update
-    post_id = params[:post_id]
-    title = params[:title]
-    detail = params[:detail]
-
-    post = Post.find(post_id)
-    post.title = title
-    post.detail = detail
+    post = post_by_id
     
-    if post.update
+    if post.update(post_params)
       flash[:success] = "게시글을 수정하셨습니다!"
-      redirect_to '/show/' + post_id
+      redirect_to post_path(post)
     else
       flash[:alert] = "게시글 수정에 실패하였습니다!"
-      redirect_to '/show/' + post_id
+      redirect_to post_path(post)
     end
 end
 
   def destroy
-    post_id = params[:post_id]
-    post = Post.find(post_id)
+    post = post_by_id
     
     if post.destroy
       flash[:success] = "게시글을 삭제하셨습니다!"
-      redirect_to '/'
+      redirect_to posts_path
     else
       flash[:alert] = "게시글 삭제에 실패하였습니다!"
-      redirect_to '/'
+      redirect_to posts_path
     end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :detail)
+  end
+
+  def post_by_id
+    Post.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,10 @@
 Rails.application.routes.draw do
   
-  get '/' => 'posts#index'
-  get 'posts'  => 'posts#index'
-  get 'posts/new'
-  post 'posts' => 'posts#create'
-  get 'posts/:post_id' => 'posts#show'
-  get 'posts/:post_id/edit' => 'posts#edit'
-  patch 'posts/:post_id' => 'posts#update'
-  delete 'posts/:post_id' => 'posts#destroy'
+  root 'posts#index'
 
-  post 'posts/:post_id/comments' => 'comments#create'
-  patch 'posts/:post_id/comments/:comment_id' => 'comments#update'
-  delete 'posts/:post_id/comments/:comment_id' => 'comments#destroy'
+  resources :posts do
+    resources :comments
+  end
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
